bdd4046af08039bc4f22fc502a07fbb546866379
Karol.Staszewski <karol.staszewski@daw-systems.pl>
2021-04-09 14:52:19 +0200

    Usunięcie projektu bazodanowego bo niepotrzebny.

--- WBZ/SQL/database.sql
+++ WBZ/SQL/database.sql
@@ -19,265 +19,230 @@ CREATE ROLE wbz_user WITH
 
 -- SCHEMA: wbz
 CREATE SCHEMA wbz AUTHORIZATION postgres;
-COMMENT ON SCHEMA wbz IS 'Wielkopolski Bank ¯ywnoœci';
 GRANT ALL ON SCHEMA wbz TO postgres;
 GRANT ALL ON SCHEMA wbz TO wbz_user;
 
 -- Table: wbz.config
 CREATE TABLE wbz.config
 (
-    property character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    value character varying(100) COLLATE pg_catalog."default",
-    CONSTRAINT config_pkey PRIMARY KEY (property)
-)
-TABLESPACE pg_default;
+    property character varying(50) PRIMARY KEY,
+    value character varying(100)
+);
 
 INSERT INTO wbz.config (property, value) VALUES
     ('VERSION', '1.2.0'),
-    ('LOGS_ENABLED', '0'),
     ('EMAIL_HOST', 'smtp.gmail.com'),
 	('EMAIL_PORT', '587'),
 	('EMAIL_ADDRESS', 'wbz.email.testowy@gmail.com'),
-	('EMAIL_PASSWORD', '');
+	('EMAIL_PASSWORD', ''),
+    ('LOGS_ENABLED', '0');
 
 -- Table: wbz.users
 CREATE TABLE wbz.users
 (
-    id serial NOT NULL,
-    username character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    password text COLLATE pg_catalog."default" NOT NULL,
-    email character varying(60) COLLATE pg_catalog."default" NOT NULL,
-    phone character varying(16) COLLATE pg_catalog."default",
-    forename character varying(30) COLLATE pg_catalog."default",
-    lastname character varying(30) COLLATE pg_catalog."default",
+    id serial PRIMARY KEY,
+    username character varying(20) NOT NULL UNIQUE,
+    password text NOT NULL,
+    email character varying(60) NOT NULL UNIQUE,
+    phone character varying(16),
+    forename character varying(30),
+    lastname character varying(30),
     blocked boolean NOT NULL DEFAULT false,
-    archival boolean NOT NULL DEFAULT false,
-    CONSTRAINT users_pkey PRIMARY KEY (id),
-    CONSTRAINT users_username_key UNIQUE (username),
-    CONSTRAINT users_email_key UNIQUE (email)
-)
-TABLESPACE pg_default;
+    archival boolean NOT NULL DEFAULT false
+);
 
 -- Table: wbz.users_permissions
 CREATE TABLE wbz.users_permissions
 (
     "user" integer NOT NULL,
-    perm character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    perm character varying(50) NOT NULL,
     CONSTRAINT users_permissions_user_fkey FOREIGN KEY ("user")
         REFERENCES wbz.users (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
         NOT VALID
-)
-TABLESPACE pg_default;
+);
 
 -- Table: wbz.logs
 CREATE TABLE wbz.logs
 (
-    id bigserial NOT NULL,
+    id bigserial PRIMARY KEY,
     "user" integer NOT NULL,
-    module character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    module character varying(50) NOT NULL,
     instance integer NOT NULL,
     type smallint NOT NULL DEFAULT 1,
-    content text COLLATE pg_catalog."default" NOT NULL,
+    content text NOT NULL,
     datetime timestamp(6) with time zone NOT NULL DEFAULT now(),
-    CONSTRAINT logs_pkey PRIMARY KEY (id),
     CONSTRAINT logs_perms_user_fkey FOREIGN KEY ("user")
         REFERENCES wbz.users (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE NO ACTION
-)
-TABLESPACE pg_default;
+);
 
 -- Table: wbz.attachments
 CREATE TABLE wbz.attachments
 (
-    id bigserial NOT NULL,
+    id bigserial PRIMARY KEY,
     "user" integer NOT NULL,
-    module character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    module character varying(50) NOT NULL,
     instance integer NOT NULL,
-    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    name character varying(50) NOT NULL,
     file bytea NOT NULL,
-    comment text COLLATE pg_catalog."default",
-    CONSTRAINT attachments_pkey PRIMARY KEY (id),
+    comment text,
     CONSTRAINT attachments_perms_user_fkey FOREIGN KEY ("user")
         REFERENCES wbz.users (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE NO ACTION
-)
-TABLESPACE pg_default;
+);
 
 -- Table: wbz.icons
 CREATE TABLE wbz.icons
 (
-    id bigserial NOT NULL,
-    module character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    "format" character varying(10) COLLATE pg_catalog."default" NOT NULL,
-    "path" character varying(250) COLLATE pg_catalog."default" NULL,
+    id bigserial PRIMARY KEY,
+    module character varying(50) NOT NULL,
+    name character varying(50) NOT NULL,
+    "format" character varying(10) NOT NULL,
+    "path" character varying(250) NULL,
     height integer NOT NULL,
     width integer NOT NULL,
     file bytea NOT NULL,
     archival boolean NOT NULL DEFAULT false,
-    comment text COLLATE pg_catalog."default",
-    CONSTRAINT icons_pkey PRIMARY KEY (id)
-)
-TABLESPACE pg_default;
+    comment text
+);
 
 -- Table: wbz.groups
 CREATE TABLE wbz.groups
 (
-    id serial NOT NULL,
-    module character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    id serial PRIMARY KEY,
+    module character varying(50) NOT NULL,
+    name character varying(50) NOT NULL,
     instance integer,
     owner integer,
     archival boolean NOT NULL DEFAULT false,
-    comment text COLLATE pg_catalog."default",
-    icon bytea,
-    CONSTRAINT groups_pkey PRIMARY KEY (id)
-)
-TABLESPACE pg_default;
+    comment text,
+    icon bytea
+);
 
 -- Table: wbz.attributes_classes
 CREATE TABLE wbz.attributes_classes
 (
-    id serial NOT NULL,
-    module character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    type character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    "values" character varying(100) COLLATE pg_catalog."default",
-    defvalue character varying(50) COLLATE pg_catalog."default",
+    id serial PRIMARY KEY,
+    module character varying(50) NOT NULL,
+    name character varying(50) NOT NULL,
+    type character varying(20) NOT NULL,
+    "values" character varying(100),
+    defvalue character varying(50),
     required boolean NOT NULL DEFAULT false,
     archival boolean NOT NULL DEFAULT false,
-    comment text COLLATE pg_catalog."default",
-    icon bytea,
-    CONSTRAINT attributes_classes_pkey PRIMARY KEY (id)
-)
-TABLESPACE pg_default;
+    comment text,
+    icon bytea
+);
 
 -- Table: wbz.attributes
 CREATE TABLE wbz.attributes
 (
-    id bigserial NOT NULL,
+    id bigserial PRIMARY KEY,
     instance integer NOT NULL,
     class integer NOT NULL,
-    value character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    value character varying(50) NOT NULL,
     archival boolean NOT NULL DEFAULT false,
-    CONSTRAINT attributes_pkey PRIMARY KEY (id),
     CONSTRAINT attributes_class_fkey FOREIGN KEY (class)
         REFERENCES wbz.attributes_classes (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
-)
-TABLESPACE pg_default;
+);
 
 -- Table: wbz.attributes
 CREATE TABLE wbz.attributes_values
 (
-    id bigserial NOT NULL,
+    id bigserial PRIMARY KEY,
     class integer NOT NULL,
-    value character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    value character varying(50) NOT NULL,
     archival boolean NOT NULL DEFAULT false,
-    CONSTRAINT attributes_values_pkey PRIMARY KEY (id),
     CONSTRAINT attributes_values_class_fkey FOREIGN KEY (class)
         REFERENCES wbz.attributes_classes (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
-)
-TABLESPACE pg_default;
+);
 
 -- Table: wbz.contacts
 CREATE TABLE wbz.contacts
 (
-    id serial NOT NULL,
-    module character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    id serial PRIMARY KEY,
+    module character varying(50) NOT NULL,
     instance integer NOT NULL,
-    email character varying(60) COLLATE pg_catalog."default",
-    phone character varying(12) COLLATE pg_catalog."default",
-    forename character varying(30) COLLATE pg_catalog."default",
-    lastname character varying(30) COLLATE pg_catalog."default",
+    email character varying(60),
+    phone character varying(12),
+    forename character varying(30),
+    lastname character varying(30),
     "default" boolean NOT NULL DEFAULT false,
-    archival boolean NOT NULL DEFAULT false,
-    CONSTRAINT contacts_pkey PRIMARY KEY (id)
-)
-TABLESPACE pg_default;
+    archival boolean NOT NULL DEFAULT false
+);
 
 -- Table: wbz.notifications
 CREATE TABLE wbz.notifications
 (
-    id serial NOT NULL,
+    id serial PRIMARY KEY,
     "user" integer NOT NULL,
-    content character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    content character varying(100) NOT NULL,
     read boolean NOT NULL DEFAULT false,
     datetime timestamp(6) with time zone NOT NULL DEFAULT now(),
-    action character varying(20) COLLATE pg_catalog."default",
-    CONSTRAINT notifications_pkey PRIMARY KEY (id),
+    action character varying(20),
     CONSTRAINT notifications_user_fkey FOREIGN KEY ("user")
         REFERENCES wbz.users (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
-)
-TABLESPACE pg_default;
+);
 
 -- Table: wbz.articles
 CREATE TABLE wbz.articles
 (
-    id serial NOT NULL,
-    codename character varying(20) COLLATE pg_catalog."default",
-    name character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    ean character varying(20) COLLATE pg_catalog."default",
+    id serial PRIMARY KEY,
+    codename character varying(20) UNIQUE,
+    name character varying(100) NOT NULL,
+    ean character varying(20),
     price numeric(16,2),
     archival boolean NOT NULL DEFAULT false,
-    comment text COLLATE pg_catalog."default",
-    icon bytea,
-    CONSTRAINT articles_pkey PRIMARY KEY (id),
-    CONSTRAINT articles_codename_key UNIQUE (codename)
-)
-TABLESPACE pg_default;
+    comment text,
+    icon bytea
+);
 
 -- Table: wbz.articles_measures
 CREATE TABLE wbz.articles_measures
 (
-    id serial NOT NULL,
+    id serial PRIMARY KEY,
     article integer NOT NULL,
-    name character varying(10) COLLATE pg_catalog."default" NOT NULL,
+    name character varying(10) NOT NULL,
     converter double precision NOT NULL DEFAULT 1,
     "default" boolean NOT NULL DEFAULT false,
-    CONSTRAINT articles_measures_pkey PRIMARY KEY (id),
     CONSTRAINT articles_measures_article_fkey FOREIGN KEY (article)
         REFERENCES wbz.articles (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
-)
-TABLESPACE pg_default;
+);
 
 -- Table: wbz.stores
 CREATE TABLE wbz.stores
 (
-    id serial NOT NULL,
-    codename character varying(20) COLLATE pg_catalog."default",
-    name character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    city character varying(40) COLLATE pg_catalog."default" NOT NULL,
-    address character varying(60) COLLATE pg_catalog."default" NOT NULL,
-    postcode character varying(6) COLLATE pg_catalog."default" NOT NULL,
+    id serial PRIMARY KEY,
+    codename character varying(20) UNIQUE,
+    name character varying(100) NOT NULL,
+    city character varying(40) NOT NULL,
+    address character varying(60) NOT NULL,
+    postcode character varying(6) NOT NULL,
     archival boolean NOT NULL DEFAULT false,
-    comment text COLLATE pg_catalog."default",
-    icon bytea,
-    CONSTRAINT stores_pkey PRIMARY KEY (id),
-    CONSTRAINT stores_codename_key UNIQUE (codename)
-)
-TABLESPACE pg_default;
+    comment text,
+    icon bytea
+);
 
 -- Table: wbz.stores_articles
 CREATE TABLE wbz.stores_articles
 (
-    id serial NOT NULL,
+    id serial PRIMARY KEY,
     store integer NOT NULL,
     article integer NOT NULL,
     amount numeric(15,3) NOT NULL DEFAULT 0,
     reserved numeric(15,3) NOT NULL DEFAULT 0,
-    CONSTRAINT stores_articles_pkey PRIMARY KEY (id),
     CONSTRAINT stores_articles_store_fkey FOREIGN KEY (store)
         REFERENCES wbz.stores (id) MATCH SIMPLE
         ON UPDATE CASCADE
@@ -286,63 +251,57 @@ CREATE TABLE wbz.stores_articles
         REFERENCES wbz.articles (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
-)
-TABLESPACE pg_default;
+);
 
 -- Table: wbz.contractors
 CREATE TABLE wbz.contractors
 (
-    id serial NOT NULL,
-    codename character varying(20) COLLATE pg_catalog."default",
-    name character varying(100) COLLATE pg_catalog."default" NOT NULL,
-    branch character varying(40) COLLATE pg_catalog."default",
-    nip character varying(10) COLLATE pg_catalog."default",
-    regon character varying(9) COLLATE pg_catalog."default",
-    city character varying(40) COLLATE pg_catalog."default" NOT NULL,
-    address character varying(60) COLLATE pg_catalog."default" NOT NULL,
-    postcode character varying(6) COLLATE pg_catalog."default" NOT NULL,
+    id serial PRIMARY KEY,
+    codename character varying(20),
+    name character varying(100) NOT NULL,
+    branch character varying(40),
+    nip character varying(10),
+    regon character varying(9),
+    city character varying(40) NOT NULL,
+    address character varying(60) NOT NULL,
+    postcode character varying(6) NOT NULL,
     archival boolean NOT NULL DEFAULT false,
-    comment text COLLATE pg_catalog."default",
-    icon bytea,
-    CONSTRAINT contractors_pkey PRIMARY KEY (id)
-)
-TABLESPACE pg_default;
+    comment text,
+    icon bytea
+);
 
 -- Table: wbz.families
 CREATE TABLE wbz.families
 (
-    id serial NOT NULL,
-    declarant character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    lastname character varying(30) COLLATE pg_catalog."default" NOT NULL,
+    id serial PRIMARY KEY,
+    declarant character varying(50) NOT NULL,
+    lastname character varying(30) NOT NULL,
     members smallint NOT NULL,
-    city character varying(40) COLLATE pg_catalog."default" NOT NULL,
-    address character varying(60) COLLATE pg_catalog."default" NOT NULL,
-    postcode character varying(6) COLLATE pg_catalog."default" NOT NULL,
+    city character varying(40) NOT NULL,
+    address character varying(60) NOT NULL,
+    postcode character varying(6) NOT NULL,
     status boolean NOT NULL DEFAULT true,
     c_sms boolean NOT NULL DEFAULT false,
     c_call boolean NOT NULL DEFAULT false,
     c_email boolean NOT NULL DEFAULT false,
     archival boolean NOT NULL DEFAULT false,
-    comment text COLLATE pg_catalog."default",
-    icon bytea,
-    CONSTRAINT families_pkey PRIMARY KEY (id)
-)
-TABLESPACE pg_default;
+    comment text,
+    icon bytea
+);
 
 -- Table: wbz.documents
 CREATE TABLE wbz.documents
 (
-    id serial NOT NULL,
-    type character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    id serial PRIMARY KEY,
+    type character varying(50) NOT NULL,
+    name character varying(50) NOT NULL,
     store integer NOT NULL,
     contractor integer NOT NULL,
     dateissue date NOT NULL DEFAULT now(),
     status smallint NOT NULL DEFAULT 0,
     archival boolean NOT NULL DEFAULT false,
-    comment text COLLATE pg_catalog."default",
+    comment text,
     icon bytea,
-    CONSTRAINT documents_headers_pkey PRIMARY KEY (id),
     CONSTRAINT documents_headers_store_fkey FOREIGN KEY (store)
         REFERENCES wbz.stores (id) MATCH SIMPLE
         ON UPDATE CASCADE
@@ -351,20 +310,18 @@ CREATE TABLE wbz.documents
         REFERENCES wbz.contractors (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE NO ACTION
-)
-TABLESPACE pg_default;
+);
 
 -- Table: wbz.documents_positions
 CREATE TABLE wbz.documents_positions
 (
-    id bigserial NOT NULL,
+    id bigserial PRIMARY KEY,
     document integer NOT NULL,
     "position" smallint NOT NULL,
     article integer NOT NULL,
     amount numeric(15,3) NOT NULL DEFAULT 0,
     cost numeric(16,2) NOT NULL DEFAULT 0,
     tax numeric(16,2) NOT NULL DEFAULT 0,
-    CONSTRAINT documents_positions_pkey PRIMARY KEY (id),
     CONSTRAINT documents_positions_document_fkey FOREIGN KEY (document)
         REFERENCES wbz.documents (id) MATCH SIMPLE
         ON UPDATE CASCADE
@@ -373,27 +330,24 @@ CREATE TABLE wbz.documents_positions
         REFERENCES wbz.articles (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE NO ACTION
-)
-TABLESPACE pg_default;
+);
 
 -- Table: wbz.distributions
 CREATE TABLE wbz.distributions
 (
-    id serial NOT NULL,
-    name character varying(50) COLLATE pg_catalog."default",
+    id serial PRIMARY KEY,
+    name character varying(50),
     datereal date,
     status smallint NOT NULL DEFAULT 0,
     archival boolean NOT NULL,
-    comment text COLLATE pg_catalog."default",
-    icon bytea,
-    CONSTRAINT distribution_pkey PRIMARY KEY (id)
-)
-TABLESPACE pg_default;
+    comment text,
+    icon bytea
+);
 
 -- Table: wbz.distributions_positions
 CREATE TABLE wbz.distributions_positions
 (
-    id bigserial NOT NULL,
+    id bigserial PRIMARY KEY,
     distribution integer NOT NULL,
     "position" smallint,
     family integer NOT NULL,
@@ -402,7 +356,6 @@ CREATE TABLE wbz.distributions_positions
     article integer NOT NULL,
     amount numeric(15,3) NOT NULL DEFAULT 0,
     status smallint NOT NULL,
-    CONSTRAINT distributions_positions_pkey PRIMARY KEY (id),
     CONSTRAINT distributions_positions_distribution_fkey FOREIGN KEY (distribution)
         REFERENCES wbz.distributions (id) MATCH SIMPLE
         ON UPDATE CASCADE
@@ -419,45 +372,40 @@ CREATE TABLE wbz.distributions_positions
         REFERENCES wbz.articles (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE NO ACTION
-)
-TABLESPACE pg_default;
+);
 
 -- Table: wbz.employees
 CREATE TABLE wbz.employees
 (
-    id serial NOT NULL,
-    email character varying(60) COLLATE pg_catalog."default",
-    phone character varying(16) COLLATE pg_catalog."default",
-    forename character varying(50) COLLATE pg_catalog."default" NOT NULL,
-    lastname character varying(30) COLLATE pg_catalog."default" NOT NULL,
-    city character varying(40) COLLATE pg_catalog."default",
-    address character varying(60) COLLATE pg_catalog."default",
-    postcode character varying(6) COLLATE pg_catalog."default",
-    department character varying(40) COLLATE pg_catalog."default",
-    position character varying(40) COLLATE pg_catalog."default",
+    id serial PRIMARY KEY,
+    email character varying(60),
+    phone character varying(16),
+    forename character varying(50) NOT NULL,
+    lastname character varying(30) NOT NULL,
+    city character varying(40),
+    address character varying(60),
+    postcode character varying(6),
+    department character varying(40),
+    position character varying(40),
     archival boolean NOT NULL DEFAULT false,
-    comment text COLLATE pg_catalog."default",
-    icon bytea,
-    CONSTRAINT employees_pkey PRIMARY KEY (id)
-)
-TABLESPACE pg_default;
+    comment text,
+    icon bytea
+);
 
 -- Table: wbz.vehicles
 CREATE TABLE wbz.vehicles
 (
-    id serial NOT NULL,
-    register character varying(20) COLLATE pg_catalog."default" NOT NULL,
-    brand character varying(40) COLLATE pg_catalog."default",
-    model character varying(60) COLLATE pg_catalog."default",
+    id serial PRIMARY KEY,
+    register character varying(20) NOT NULL UNIQUE,
+    brand character varying(40),
+    model character varying(60),
     capacity decimal(18,3),
     forwarder integer,
     driver integer,
     prodyear integer,
     archival boolean NOT NULL DEFAULT false,
-    comment text COLLATE pg_catalog."default",
+    comment text,
     icon bytea,
-    CONSTRAINT vehicles_pkey PRIMARY KEY (id),
-    CONSTRAINT vehicles_register_key UNIQUE (register),
     CONSTRAINT vehicles_headers_forwarder_fkey FOREIGN KEY (forwarder)
         REFERENCES wbz.contractors (id) MATCH SIMPLE
         ON UPDATE CASCADE
@@ -466,8 +414,7 @@ CREATE TABLE wbz.vehicles
         REFERENCES wbz.employees (id) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE NO ACTION
-)
-TABLESPACE pg_default;
+);
 
 -- FUNCTION: wbz.artdefmeaval(integer, double precision)
 CREATE OR REPLACE FUNCTION wbz.artdefmeaval(
