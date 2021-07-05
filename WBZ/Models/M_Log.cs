b3aec4ae0eee8001b0f0b9677c7b5d37975339de
Karol Staszewski <karol.staszewski@daw-systems.pl>
2021-07-05 16:00:52 +0200

    Dodanie zakładek z opisami (jeszcze bez ikony).

--- WBZ/Models/M_Log.cs
+++ WBZ/Models/M_Log.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using WBZ.Globals;
 
 namespace WBZ.Models
 {
---
+++ WBZ/Modules/_shared/DescriptionsTab.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WBZ.Models;
+
+namespace WBZ.Modules._shared
+{
+    /// <summary>
+    /// Interaction logic for DescriptionsTab.xaml
+    /// </summary>
+    public partial class DescriptionsTab : UserControl
+    {
+        public DescriptionsTab()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Module
+        /// </summary>
+        public static readonly DependencyProperty ModuleProperty
+            = DependencyProperty.Register(
+                  nameof(Module),
+                  typeof(MV),
+                  typeof(DescriptionsTab),
+                  new PropertyMetadata(default(MV))
+              );
+        public MV Module
+        {
+            get => (MV)GetValue(ModuleProperty);
+            set => SetValue(ModuleProperty, value);
+        }
+
+        /// <summary>
+        /// InstanceID
+        /// </summary>
+        public static readonly DependencyProperty InstanceIDProperty
+            = DependencyProperty.Register(
+                  nameof(InstanceID),
+                  typeof(int),
+                  typeof(DescriptionsTab),
+                  new PropertyMetadata(default(int))
+              );
+        public int InstanceID
+        {
+            get => (int)GetValue(InstanceIDProperty);
+            set => SetValue(InstanceIDProperty, value);
+        }
+    }
+}
